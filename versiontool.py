378062c2929214e044525dce7259f201b160f153
System Failure <100814399+the-real-millySAYhi@users.noreply.github.com>
2022-05-09 16:37:31 -0700

    Changed the version number.
    Changed the version number to 1.2 and added a Beta option for use in the beta branch as well as rearranging the code for better readability.

--- versiontool.py
+++ versiontool.py
@@ -1,8 +1,10 @@
+#-------change this----------------
+version = ("1.2 Beta")
+#----------------------------------
+
 raw = open("Root file directory.fdir","r")
 root = raw.read()
 path = (root + "\Software data\ ")
-
-version = ("1.0")
 
 softwareV = open(path + "version.info","w")
 softwareV.write(version)
