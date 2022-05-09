a96f79feb6d64713a31b0c9ebfe103628b0df5ec
System Failure <100814399+the-real-millySAYhi@users.noreply.github.com>
2022-05-09 16:42:07 -0700

    Fixed the last commit.
    Fixed the Beta ability in the last commit.

--- versiontool.py
+++ versiontool.py
@@ -1,6 +1,13 @@
-#-------change this----------------
-version = ("1.2 Beta")
+#-------change these---------------
+version = ("1.2")
+Beta = (true)
 #----------------------------------
+
+
+if(Beta == true):
+  trueversion = (version + " Beta")
+else:
+  trueversion = (version)
 
 raw = open("Root file directory.fdir","r")
 root = raw.read()
